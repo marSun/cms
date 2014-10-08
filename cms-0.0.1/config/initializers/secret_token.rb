c14386c7d03e190fa2cbea9f054edbd5332b7255
tianji <tianji@tianji-desktop.(none)>
2014-10-08 17:05:01 +0800

    init

---
+++ cms-0.0.1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms::Application.config.secret_token = '695036d76c838b1885e472f44cbbe37fed8387c667ce0d4698af02e79dc993f7e9262a2d3bcf67dd1dd735dc2e2a1051282f60e39fb3943cf3ccf68a0a2fdef2'
